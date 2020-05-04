59574d0d6f334fff8ac5489db5a226042b327957
Ramce <ramuzuconcepcion@gmail.com>
2020-05-04 12:31:42 +0800

    Added nuxt template, added sass loader

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <Header />
+    <Sidebar />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Header from "~/components/Header";
+import Sidebar from "~/components/Sidebar";
+
+export default Vue.extend({
+  components: {
+    Header,
+    Sidebar
+  }
+});
+</script>
+
+<style lang="scss">
+body {
+  padding: 0;
+  margin: 0;
+}
+
+.app {
+}
+</style>
