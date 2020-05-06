923e079a9f06a5a4010fdfdedc8c20b8629cf99b
Ramce <ramuzuconcepcion@gmail.com>
2020-05-06 18:15:27 +0800

    Fixed auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,13 @@
+function is(group, url) {
+    const paths = url.split("/");
+    return paths[1] === group;
+}
+
 module.exports = function (req, res, next) {
     const sess = req.session;
 
-    if (!sess.userId && req.url !== "/login") {
+    //redirect to login if userId does not exist, url is not login and the url is not API group
+    if (!sess.userId && req.url !== "/login" && is("api", req.url) === false) {
         res.redirect('/login');
     }
     else next();
--- server/api/Auth/AuthRouter.js
+++ server/api/Auth/AuthRouter.js
@@ -3,13 +3,6 @@ const router = express.Router();
 
 const AuthController = require('./AuthController.js');
 
-router.post('/', async (req, res, next) => {
-    const sqlmanager = req.app.locals.sqlmanager;
-    const controller = new AuthController(sqlmanager);
-
-    res.send(req.session.userId);
-});
-
 router.post('/login', async (req, res, next) => {
     const sqlmanager = req.app.locals.sqlmanager;
     const controller = new AuthController(sqlmanager);
