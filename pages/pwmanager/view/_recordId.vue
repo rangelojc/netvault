80ecda09fee4eaae00b04b90f4910fa3bce9ff0d
Ramce <ramuzuconcepcion@gmail.com>
2020-05-18 12:41:29 +0800

    Changed routing structure, wip pwmanager record view page

---
+++ pages/pwmanager/view/_recordId.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>Hello</div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            recordId: 0
+        };
+    },
+    mounted() {
+        this.data.recordId = this.$route.params.recordId;
+    }
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/pwmanager/view/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+
+
+<script>
+export default {
+    mounted() {
+        this.$router.push("/pwmanager");
+    }
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style>
